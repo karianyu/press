9420252f37798fe85bf468b4d4a785dfa130d8ed
Aditya Hase <aditya@adityahase.com>
2025-01-17 17:02:22 +0530

    fix(agent): Use server instead of server_name

--- press/press/report/server_stats/server_stats.py
+++ press/press/report/server_stats/server_stats.py
@@ -164,14 +164,10 @@ def get_data(filters):
 			"cpu": available_data.get("vcpu", 0),
 			"cpu_used": rounded(used_data.get("vcpu", 0) * 100, 1),
 			"disk": rounded(available_data.get("disk", 0), 2),
-			"disk_used": rounded(
-				(used_data.get("disk", 0) / available_data.get("disk", 1)) * 100, 1
-			),
+			"disk_used": rounded((used_data.get("disk", 0) / available_data.get("disk", 1)) * 100, 1),
 			"disk_free": available_data.get("disk", 0) - used_data.get("disk", 0),
 			"memory": rounded(available_data.get("memory", 0) / 1024, 2),
-			"memory_used": rounded(
-				(used_data.get("memory", 0) / available_data.get("memory", 1)) * 100, 1
-			),
+			"memory_used": rounded((used_data.get("memory", 0) / available_data.get("memory", 1)) * 100, 1),
 			"swap": rounded(swap_memory.get("swap", 0), 1),
 			"swap_used": rounded(swap_memory.get("swap_used", 0), 1),
 			"memory_required": rounded(swap_memory.get("required", 0), 1),
@@ -190,8 +186,7 @@ def get_data(filters):
 					"new_worker_allocation": frappe.db.get_value(
 						server.server_type, server.name, "new_worker_allocation"
 					),
-					"ram_assigned": (frappe.db.get_value(server.server_type, server.name, "ram") or 0)
-					/ 1024,
+					"ram_assigned": (frappe.db.get_value(server.server_type, server.name, "ram") or 0) / 1024,
 				}
 			)
 		rows.append(row)
@@ -202,9 +197,9 @@ def get_data(filters):
 def get_servers(filters):
 	server_filters = {"status": "Active", **filters}
 
-	if filters.server_name:
-		server_filters["name"] = ("like", f"%{filters.server_name}%")
-	server_filters.pop("server_name", None)
+	if filters.server:
+		server_filters["name"] = ("like", f"%{filters.server}%")
+	server_filters.pop("server", None)
 
 	if filters.exclude_self_hosted:
 		server_filters["is_self_hosted"] = False
