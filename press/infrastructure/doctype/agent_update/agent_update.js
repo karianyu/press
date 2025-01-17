9543a0522809276a50aa7d0252806022cb7c95f4
Aditya Hase <aditya@adityahase.com>
2025-01-17 19:45:44 +0530

    feat(agent): Pick servers based on filters
    Uses the same filters as Server Stats and Agent Versions report

--- press/infrastructure/doctype/agent_update/agent_update.js
+++ press/infrastructure/doctype/agent_update/agent_update.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2025, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Agent Update", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Agent Update", {
+    refresh(frm) {
+        ["team", "cluster", "server_type", "server", "exclude_self_hosted"].forEach(field => {
+            if (!frm.is_new()) {
+                frm.set_df_property(field, "get_status", () => "Read");
+            }
+        });
+    },
+});
--- press/infrastructure/doctype/agent_update/agent_update.py
+++ press/infrastructure/doctype/agent_update/agent_update.py
@@ -9,6 +9,7 @@ from frappe.model.document import Document
 
 from press.press.doctype.app_source.app_source import get_timestamp_from_commit_info
 from press.press.doctype.server.server import BaseServer
+from press.press.report.server_stats.server_stats import get_servers
 
 
 class AgentUpdate(Document):
@@ -20,13 +21,21 @@ class AgentUpdate(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		from press.infrastructure.doctype.agent_update_server.agent_update_server import AgentUpdateServer
+
 		agent_repository_branch: DF.Data | None
 		agent_repository_url: DF.Data | None
+		cluster: DF.Link | None
 		commit_author: DF.Data | None
 		commit_hash: DF.Data | None
 		commit_message: DF.Code | None
 		commit_timestamp: DF.Datetime | None
+		exclude_self_hosted: DF.Check
 		name: DF.Int | None
+		server: DF.Data | None
+		server_type: DF.Link | None
+		servers: DF.Table[AgentUpdateServer]
+		team: DF.Link | None
 	# end: auto-generated types
 
 	pass
@@ -34,6 +43,7 @@ class AgentUpdate(Document):
 	def before_insert(self):
 		self.set_agent_repository()
 		self.fetch_latest_commit()
+		self.set_servers()
 
 	def set_agent_repository(self):
 		self.agent_repository_url = BaseServer.get_agent_repository_url().removesuffix(".git")
@@ -50,6 +60,18 @@ class AgentUpdate(Document):
 		self.commit_message = commit.message
 		self.commit_author = commit.author
 		self.commit_timestamp = commit.timestamp
+
+	def get_filters_dict(self):
+		filters = frappe._dict()
+		for field in ["team", "cluster", "server_type", "server_name", "exclude_self_hosted"]:
+			if value := self.get(field):
+				filters[field] = value
+		return filters
+
+	def set_servers(self):
+		filters = self.get_filters_dict()
+		for server in get_servers(filters):
+			self.append("servers", {"server_type": server.server_type, "server": server.name})
 
 
 class GitHubCommit:
